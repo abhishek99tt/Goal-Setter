3d5a14e5d3e9bbd2a22125656780b229108c32af
abhishek99tt <singhabhishek8080@gmail.com>
2023-01-26 18:14:27 +0530

    Rest API for goal

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.port || 5000;
+const Goal = require('./models/goalModel')
+
+const connectDB = require('./config/db')
+connectDB()
+
 const app = express();
 
 app.use(express.json())
