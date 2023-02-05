c21ebafdf3c31c2033fd338d33677dac8ffdf7bd
abhishek99tt <singhabhishek8080@gmail.com>
2023-02-05 16:54:41 +0530

    Goal setter app completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -12,7 +12,7 @@ const getGoals = asyncHandler(async(req,res) => {
 })
 
 // @desc        SET Goal
-// @route       POSt /api/goal
+// @route       POSt /api/goals
 // @access      Private
 const setGoal = asyncHandler(async(req,res) => {
     if(!req.body.text){
@@ -39,16 +39,14 @@ const updateGoal = asyncHandler(async(req,res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
     // Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not Authorized')
     }
@@ -72,16 +70,15 @@ const deleteGoal = asyncHandler(async(req,res) => {
         throw new Error('Goal not found')
     } 
 
-    const user = await User.findById(req.user.id) 
 
         // Check for user
-        if(!user) {
+        if(!req.user) {
             res.status(401)
             throw new Error('User not found')
         }
     
         // Make sure the logged in user matches the goal user
-        if(goal.user.toString() !== user.id) {
+        if(goal.user.toString() !== req.user.id) {
             res.status(401)
             throw new Error('User not Authorized')
         }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react"
+import { useSelector, useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,13 +15,49 @@ const Register = () => {
   })
   const { name, email, password, password2 } = formData;
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  const { user, isLoading, isSuccess, isError, message } = useSelector((state) => state.auth)
+
+
   const onChange = ({target}) => {
     setFormData((prevState) => ({...prevState, [target.name] : target.value}))
   }
 
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset)
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
   const onSubmit = (e) => {
     e.preventDefault();
+    
+    if(password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+
+      dispatch(register(userData))
+
+    }
   }
+
+  if(isLoading) {
+    return <Spinner />
+  }
+
   return (
     <>
     <section className="heading">
